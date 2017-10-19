53851422197f749bc3d54765e8b68a38da215b9b
aggg6 <aggg6@mail.umkc.edu>
2017-10-19 17:41:24 -0500

    get method added

--- server/server.js
+++ server/server.js
@@ -23,6 +23,14 @@ app.post('/todos',(req,res)=>{
   });
 });
 
+app.get('/todos',(req,res)=>{
+  Todo.find().then((result)=>{
+    res.send({result});
+  },(e)=>{
+    res.status(400).send(e);
+  })
+})
+
 
 app.listen(3000,()=>{
   console.log('Server is up on port 3000');
