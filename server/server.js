ce92b4ed67b65d9e2ec7aebe5be602ca6765a334
aggg6 <aggg6@mail.umkc.edu>
2017-10-19 21:20:11 -0500

    deploy changes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -46,7 +47,7 @@ app.get('/todos/:id',(req,res)=>{
 })
 
 app.listen(3000,()=>{
-  console.log('Server is up on port 3000');
+  console.log(`Server is up on port: ${port}`);
 })
 
 module.exports = {app} ;
