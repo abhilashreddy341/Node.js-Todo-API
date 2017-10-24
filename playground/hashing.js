7b0661b795866466b11b0b4a0f5dc7965a464d50
aggg6 <aggg6@mail.umkc.edu>
2017-10-24 12:09:03 -0500

    passwords hashed

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,19 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcryptjs = require('bcryptjs');
+
+var password = '123abc';
+
+bcryptjs.genSalt(10,(err,salt)=>{
+  bcryptjs.hash(password,salt,(err,hash)=>{
+    console.log(hash);
+
+  bcryptjs.compare(password,hash,(err,res)=>{
+    console.log(res);
+  });
+  })
+});
+
 
 // var message = 'I am doing good';
 // var hash = SHA256(message).toString();
@@ -7,11 +21,11 @@ const jwt = require('jsonwebtoken');
 // console.log(`message : ${message}`);
 // console.log(`hash : ${hash}`);
 
-var data = {
-  id : 18,
-}
-
-var hash = jwt.sign(data.id,'12345').toString();
-console.log(hash);
-var decode = jwt.verify(hash,'1234');
-console.log(decode);
+// var data = {
+//   id : 18,
+// }
+//
+// var hash = jwt.sign(data.id,'12345').toString();
+// console.log(hash);
+// var decode = jwt.verify(hash,'1234');
+// console.log(decode);
